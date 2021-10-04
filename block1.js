cbc36223c5374e33232c3e98bf010d7e93e91b11
rupak277 <83598878+rupak277@users.noreply.github.com>
2021-10-04 17:04:13 +0530

    Add files via upload

---
+++ block1.js
@@ -0,0 +1,34 @@
+class Block1{
+    constructor(x,y,width,height){
+        var options = {
+            
+            restituition: 0.4,
+            friction:0.0
+        }
+        
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world, this.body);
+
+    }
+
+    display(){
+        
+        var angle = this.body.angle;
+        var pos= A
+        var pos= this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+         
+        fill(125,200,225);
+        visualViewport.offsetLeft
+        strokeWeight(1.5);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+     
+        
+    }
+}
